f3c59e8dd9eefcc1556811d07cbc0d6f35c32252
Jack Danger Canty <gitcommit@6brand.com>
2010-01-06 14:27:20 -0800

    appending 'silk' to image names

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 
 require 'fileutils'
-FileUtils.cp File.join(File.dirname(__FILE__), 'famfamfam.png'),
-             File.join('public', 'images', 'famfamfam.png')
+FileUtils.cp File.join(File.dirname(__FILE__), 'famfamfam_silk.png'),
+             File.join('public', 'images', 'famfamfam_silk.png')
 
 FileUtils.cp File.join(File.dirname(__FILE__), 'blank.gif'),
              File.join('public', 'images', 'blank.gif')
